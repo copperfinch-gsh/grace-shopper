4c7c15d8f88be09ec4ea0685b7a54ed67abca8b0
Saeed Sheikh <saeedshaikh166@gmail.com>
2019-04-16 11:18:12 -0400

    Adds .env file and requires dotenv package in server index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,10 @@ if (process.env.NODE_ENV === 'test') {
  * keys as environment variables, so that they can still be read by the
  * Node process on process.env
  */
-if (process.env.NODE_ENV !== 'production') require('../secrets')
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv')
+  dotenv.config()
+}
 
 // passport registration
 passport.serializeUser((user, done) => done(null, user.id))
