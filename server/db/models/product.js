0f28a18c3dbe2f797130272343064f11ad1fae56
Roumesh Persand <rpersand25@gmail.com>
2019-04-15 22:01:55 -0400

    added validation for product name

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,33 +1,37 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require('sequelize')
+const db = require('../db')
 
 const Product = db.define('product', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    imageUrl: {
-        type: Sequelize.STRING,
-        defaultValue: 'https://www.long-mcquade.com/images/departmentHeaders/department_5.jpg'
-    },
-    description: {
-        type: Sequelize.TEXT
-    },
-    type: {
-        type: Sequelize.STRING
-    },
-    price: {
-        type: Sequelize.FLOAT
-    },
-    color: {
-        type: Sequelize.STRING
-    },
-    manufacturer: {
-        type: Sequelize.STRING
-    },
-    quantity: {
-        type: Sequelize.INTEGER
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
     }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://www.long-mcquade.com/images/departmentHeaders/department_5.jpg'
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  type: {
+    type: Sequelize.STRING
+  },
+  price: {
+    type: Sequelize.FLOAT
+  },
+  color: {
+    type: Sequelize.STRING
+  },
+  manufacturer: {
+    type: Sequelize.STRING
+  },
+  quantity: {
+    type: Sequelize.INTEGER
+  }
 })
 
-module.exports = Product;
+module.exports = Product
