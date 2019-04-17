fe24e8839dee1fcac2695d16d7f0eae397d4e393
Harrison Cole <harrison.d.cole@gmail.com>
2019-04-16 21:25:32 -0400

    convert Cart to functional component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,53 +1,43 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 
-const sampleCart = [
-  {id: 1, name: 'Strat', imageUrl: '', price: 20, quantity: 1}
-]
-
-class Cart extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div className="container-fluid">
-        <style jsx>{`
-          h3 {
-            color: green;
-          }
-        `}</style>
-        <h3>Shopping Cart:</h3>
-        {this.props.cart.map(item => {
-          return (
-            <div key={item.id}>
-              <div>
-                {' '}
-                <img src={item.imageUrl} alt="item image" />{' '}
-              </div>
-              <div> name: {item.name} </div>
-              <div> price: {item.price} </div>
-              <div>
-                {' '}
-                <select>
-                  <option value="1" selected>
-                    1
-                  </option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                </select>{' '}
-              </div>
+const Cart = ({cartItems}) => {
+  return (
+    <div className="container-fluid">
+      <style jsx>{`
+        h3 {
+          color: green;
+        }
+      `}</style>
+      <h3>Shopping Cart:</h3>
+      {cartItems.map(item => {
+        return (
+          <div key={item.id}>
+            <div>
+              {' '}
+              <img src={item.imageUrl} alt="item image" />{' '}
             </div>
-          )
-        })}
-      </div>
-    )
-  }
+            <div> name: {item.name} </div>
+            <div> price: {item.price} </div>
+            <div>
+              {' '}
+              <select>
+                <option value="1" selected>
+                  1
+                </option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>{' '}
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 const mapStateToProps = state => ({
-  cart: state.cart
+  cartItems: state.cart
 })
 
 export default connect(mapStateToProps)(Cart)
