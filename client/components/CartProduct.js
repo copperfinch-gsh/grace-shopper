4bd8570a96e6fb218ab681d83ae5fb5e8678f4af
Ariel Ahdoot <arieli612@aol.com>
2019-04-17 21:50:46 -0400

    extracted CartProduct component from Cart

---
+++ client/components/CartProduct.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const CartProduct = ({ item, handleChange }) => {
+  return (
+    <div>
+      <div>
+        {' '}
+        <img src={item.imageUrl} alt="item image" />{' '}
+      </div>
+      <div> name: {item.name} </div>
+      <div> price: {item.price * item.desiredQuantity} </div>
+      <div>
+        {' '}
+        <select onChange={event => handleChange(event, item)}>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(number => {
+            return (
+              <option
+                key={number}
+                value={number}
+                selected={
+                  item.desiredQuantity &&
+                  number === Number(item.desiredQuantity)
+                    ? 'selected'
+                    : ''
+                }
+              >
+                {number}
+              </option>
+            );
+          })}
+        </select>{' '}
+      </div>
+    </div>
+  );
+};
+
+export default CartProduct;
