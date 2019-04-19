41420eb4f118572f81176c00d1e038762c95fcba
Harrison Cole <harrison.d.cole@gmail.com>
2019-04-19 16:14:25 -0400

    isCart boolean on order model

--- server/api/orders.js
+++ server/api/orders.js
@@ -13,15 +13,22 @@ router.get('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
+    //one item at a time coming to post route
     const items = req.body.items;
-    const order = await Order.create();
+    // const order = await Order.create();
 
     //grab user
     if (req.user) {
-      await order.setUser(req.user);
+      // await order.setUser(req.user);
+      //find 'cart' order if it exists
+      //if cart order exists, set ordererId variable for lineitem to that orderid
+      //else create new order and set this user to the order
     }
 
+    //if there is no user on the session, // does req.session.cart.id exist ? if it does, find that order and add to it below. if it doesn't, create the order and set req.session.card.id to that order id
+
     //create new items array, only taking the product id from the items array in the req body
+
     let lineItems = [];
 
     for (let i = 0; i < items.length; i++) {
