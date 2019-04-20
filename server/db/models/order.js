4650abcd341a6b12988d481fb49659c4f5401302
Ariel Ahdoot <arieli612@aol.com>
2019-04-19 20:24:30 -0400

    added getFullCart() function and test

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
+const Product = db.model('product');
 
 const Order = db.define('order', {
   isCart: {
@@ -10,6 +11,18 @@ const Order = db.define('order', {
 
 Order.findCart = async function(userId) {
   const cart = await this.findOne({
+    where: {
+      userId: userId,
+      isCart: true
+    }
+  });
+
+  return cart;
+};
+
+Order.getFullCart = async function(userId) {
+  const cart = await this.findOne({
+    include: [{ model: Product }],
     where: {
       userId: userId,
       isCart: true
--- server/db/models/order.spec.js
+++ server/db/models/order.spec.js
@@ -3,8 +3,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 const db = require('../index');
-const Order = db.model('order');
-const User = db.model('user');
+const { Order, User, Product, LineItem } = require('./index');
 
 describe('Order model', () => {
   beforeEach(() => {
@@ -32,6 +31,70 @@ describe('Order model', () => {
 
       it('returns the cart', () => {
         return Order.findCart(1).then(data => expect(data.userId).to.equal(1));
+      });
+
+      it('if the cart does not exist, return null', () => {
+        return Order.findCart(2).then(data => expect(data).to.equal(null));
+      });
+    }); // end describe('correctPassword')
+    describe('getFullCart', () => {
+      beforeEach(async () => {
+        try {
+          const cody = await User.create({
+            firstName: 'Cody',
+            lastName: 'ThePug',
+            email: 'cody@puppybook.com',
+            password: 'bones'
+          });
+
+          const order = await Order.create({});
+
+          await order.setUser(cody);
+
+          const productA = await Product.create({
+            name: 'Les Paul',
+            price: 10500,
+            quantity: 1
+          });
+          const productB = await Product.create({
+            name: 'Strat',
+            price: 30500,
+            quantity: 3
+          });
+
+          productA.desiredQuantity = 2;
+          productB.desiredQuantity = 3;
+
+          let itemA = {},
+            itemB = {};
+
+          itemA.unitPrice = productA.price;
+          itemA.orderId = order.id;
+          itemA.productId = productA.id;
+          itemA.quantity = productA.desiredQuantity;
+
+          itemB.unitPrice = productB.price;
+          itemB.orderId = order.id;
+          itemB.productId = productB.id;
+          itemB.quantity = productB.desiredQuantity;
+
+          await LineItem.create(itemA);
+          await LineItem.create(itemB);
+        } catch (error) {
+          console.error(error);
+        }
+      });
+
+      it('returns the full cart', () => {
+        return Order.getFullCart(1).then(data =>
+          expect(data.userId).to.equal(1)
+        );
+      });
+
+      it('returns the correct lineItems', () => {
+        return Order.getFullCart(1).then(data =>
+          expect(data.products[0].lineItem.unitPrice).to.equal(10500)
+        );
       });
 
       it('if the cart does not exist, return null', () => {
