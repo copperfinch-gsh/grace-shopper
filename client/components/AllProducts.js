b9bae017069b5b7a98b4327c17b3d78c7a6f8605
Roumesh Persand <rpersand25@gmail.com>
2019-04-21 14:11:04 -0400

    added a carousel

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -3,31 +3,87 @@ import { connect } from 'react-redux';
 import { getProduct } from '../store/products';
 import { addToCartThunk } from '../store/cart';
 import SingleProduct from './SingleProduct';
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
 
 class AllProducts extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      index: 0,
+      direction: null
+    };
+
+    this.handleSelect = this.handleSelect.bind(this);
   }
 
   async componentDidMount() {
     await this.props.getAllProducts();
   }
 
+  handleSelect(selectedIndex, e) {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction
+    });
+  }
+
   render() {
+    if (this.props.products[0] === undefined) {
+      console.log('loading');
+    } else {
+      console.log(this.props.products[0].imageUrl);
+    }
     const { products, addToCart } = this.props;
-    return (
-      <div id="product-container">
-        <div id="products">
-          {products.map(product => (
-            <SingleProduct
-              key={product.id}
-              product={product}
-              addToCart={addToCart}
-            />
-          ))}
+    const { index, direction } = this.state;
+
+    if (products[0] === undefined) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div id="product-container">
+          <Carousel
+            activeIndex={index}
+            direction={direction}
+            onSelect={this.handleSelect}
+          >
+            <Carousel.Item id="transparent">
+              <Image
+                className="d-block w-100"
+                src="https://steemitimages.com/DQmS753qCEwHgqkh77HJ9muSSej5LB1bR5U9Kz8RiLQ229Y/Fender-Stratocaster-Musimaster.jpg"
+                alt="First slide"
+                style={{ height: '20rem' }}
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Image
+                className="d-block w-100"
+                src="http://lincolnguitarlessons.net/wp-content/uploads/2015/06/martin-left-handed-acoustic-guitars.jpg"
+                alt="Third slide"
+                style={{ height: '20rem' }}
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <Image
+                className="d-block w-100"
+                src="http://www.stringsly.in/wp-content/uploads/2017/04/electric-guitar-brands.jpg"
+                alt="Third slide"
+                style={{ height: '20rem' }}
+              />
+            </Carousel.Item>
+          </Carousel>
+          <div id="products">
+            {products.map(product => (
+              <SingleProduct
+                key={product.id}
+                product={product}
+                addToCart={addToCart}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
