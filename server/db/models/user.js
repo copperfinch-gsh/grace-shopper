caf6b128ba6eecc03e4c3c75b78819f95c155938
Saeed Sheikh <saeedshaikh166@gmail.com>
2019-04-22 10:36:19 -0400

    Removed backend validations

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -5,17 +5,17 @@ const db = require('../db');
 const User = db.define('user', {
   firstName: {
     type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true
-    }
+    // allowNull: false,
+    // validate: {
+    //   notEmpty: true
+    // }
   },
   lastName: {
     type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true
-    }
+    // allowNull: false,
+    // validate: {
+    //   notEmpty: true
+    // }
   },
   email: {
     type: Sequelize.STRING,
